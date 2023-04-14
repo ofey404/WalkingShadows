77e32897db9e695a12885aa41cec7aec12b43885
Ofey Chan <ofey206@gmail.com>
2023-04-14 19:58:28 +0800

    stage: pytermgui

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,14 @@
-import numpy
+import time
 
-print("Hello World")
-print(f"Try numpy: {numpy.array([1, 2, 3])}")
+import pytermgui as ptg
+
+
+def macro_time(fmt: str) -> str:
+    return time.strftime(fmt)
+
+
+ptg.tim.define("!time", macro_time)
+
+with ptg.WindowManager() as manager:
+    manager.layout.add_slot("Body")
+    manager.add(ptg.Window("[bold]The current time is:[/]\n\n[!time 75]%c", box="EMPTY"))
