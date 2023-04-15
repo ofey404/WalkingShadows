d8687163c61814edd8e4cdafd5c2051e4cc32d5f
Ofey Chan <ofey206@gmail.com>
2023-04-15 22:25:33 +0800

    stage: ci and pre-commit

--- src/backend/__test__.py
+++ src/backend/__test__.py
@@ -4,13 +4,15 @@ from __init__ import app
 
 class TestServer(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app.app.test_client()
 
-    def test_get_random_number(self):
-        response = self.app.get("/note")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("message", response.json)
-        self.assertEqual("this is my message", response.json["message"])
+    def test_api_note(self):
+        response = self.app.post("/api/note", json={"message": "message"})
+        self.assertEqual(response.status_code, 200, str(response.data))
+        self.assertIn("message", response.json, str(response.data))
+        self.assertEqual(
+            "message received: message", response.json["message"], str(response.data)
+        )
 
 
 if __name__ == "__main__":
