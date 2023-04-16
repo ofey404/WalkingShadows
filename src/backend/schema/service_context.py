f2d11ae2232f30113f2cd647f720c0d800d7eacf
Ofey Chan <ofey206@gmail.com>
2023-04-17 03:46:42 +0800

    stage: singleton implementation is bad

--- src/backend/schema/service_context.py
+++ src/backend/schema/service_context.py
@@ -4,57 +4,58 @@ from langchain import LLMChain, PromptTemplate
 from langchain.llms import BaseLLM, OpenAI
 from pydantic import BaseModel
 from schema.config import Config, Secret
-from schema.singleton import Singleton
 
 
-class ServiceContext(BaseModel, Singleton):
+class ServiceContext(BaseModel):
     config: Config
     secret: Secret
     llm: BaseLLM
     chain: LLMChain
 
-
-def new_service_context(
-    config: Config,
-    secret: Secret,
-) -> ServiceContext:
-    llm = OpenAI(
-        openai_api_key=secret.openai_api_key,
-        verbose=config.debug,
-    )
-    return _new_service_context_with_llm(
-        config=config,
-        secret=secret,
-        llm=llm,
-    )
-
-
-def _new_service_context_with_llm(
-    config: Config,
-    secret: Secret,
-    llm: BaseLLM,
-) -> ServiceContext:
-    """Create a new service context"""
-    note_prompt = PromptTemplate(
-        input_variables=["message"],
-        template=inspect.cleandoc(
-            """
-            You're a philosopher in a cave, sitting beside a campfire.
-            You're thinking about the meaning of life, as the fire crackles.
-            Your friend comes up to you and give you a note, "{message}".
-            
-            What 5 high level insights can you infer from this note?
-            (example format: insight (because of reason))
-        """
-        ),
-    )
-    return ServiceContext(
-        config=config,
-        secret=secret,
-        llm=llm,
-        chain=LLMChain(
-            llm=llm,
-            prompt=note_prompt,
+    @classmethod
+    def new(
+        cls,
+        config: Config,
+        secret: Secret,
+    ) -> "ServiceContext":
+        llm = OpenAI(
+            openai_api_key=secret.openai_api_key,
             verbose=config.debug,
-        ),
-    )
+        )
+        return cls.new_with_llm(
+            config=config,
+            secret=secret,
+            llm=llm,
+        )
+
+    @classmethod
+    def new_with_llm(
+        cls,
+        config: Config,
+        secret: Secret,
+        llm: BaseLLM,
+    ) -> "ServiceContext":
+        """Create a new service context"""
+        note_prompt = PromptTemplate(
+            input_variables=["message"],
+            template=inspect.cleandoc(
+                """
+                You're a philosopher in a cave, sitting beside a campfire.
+                You're thinking about the meaning of life, as the fire crackles.
+                Your friend comes up to you and give you a note, "{message}".
+                
+                What 5 high level insights can you infer from this note?
+                (example format: insight (because of reason))
+            """
+            ),
+        )
+        return cls(
+            config=config,
+            secret=secret,
+            llm=llm,
+            chain=LLMChain(
+                llm=llm,
+                prompt=note_prompt,
+                verbose=config.debug,
+            ),
+        )
--- src/backend/schema/test/__init__.py
+++ src/backend/schema/test/__init__.py
@@ -2,11 +2,10 @@ from typing import List
 
 from langchain.llms.fake import FakeListLLM
 from schema import Config, Secret, ServiceContext
-from schema.service_context import _new_service_context_with_llm
 
 
 def fake_service_context(llm_responses: List[str]) -> ServiceContext:
-    return _new_service_context_with_llm(
+    return ServiceContext.new_with_llm(
         config=Config(
             port=5000,
             debug=True,
