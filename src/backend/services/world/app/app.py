223ddc39374459d5efc36ae8013db48e6aeb757b
Ofey Chan <ofey206@gmail.com>
2023-05-07 18:55:56 +0800

    stage: debug docker compose

--- src/backend/services/world/app/app.py
+++ src/backend/services/world/app/app.py
@@ -1,8 +1,26 @@
+from contextlib import asynccontextmanager
+
 from fastapi import APIRouter, FastAPI
+from libs import mongox
+from services.world.internal import models
+from services.world.settings import get_settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    s = get_settings()
+    await mongox.init(
+        settings=s.mongo,
+        document_models=[
+            models.World,
+            models.Character,
+        ],
+    )
+    yield
 
 
 def create_app(*routers: APIRouter) -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     for r in routers:
         app.include_router(r)
