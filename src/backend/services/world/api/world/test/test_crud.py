4213968d73174958f99acbd82042b2a5b0d3f4bb
Ofey Chan <ofey206@gmail.com>
2023-05-08 18:54:16 +0800

    stage: before IsolatedAsyncioTestCase

--- src/backend/services/world/api/world/test/test_crud.py
+++ src/backend/services/world/api/world/test/test_crud.py
@@ -1,5 +1,6 @@
 import unittest
 
+import fastapi
 from fastapi.testclient import TestClient
 from libs import mongox
 from libs.testx import MONGO_CONTAINER_VERSION
@@ -11,38 +12,49 @@ from testcontainers.mongodb import MongoDbContainer
 
 
 class TestCrud(unittest.IsolatedAsyncioTestCase):
-    async def asyncSetUp(self) -> None:
-        self.mongo = MongoDbContainer(MONGO_CONTAINER_VERSION)
-        self.mongo.start()
+    mongo: MongoDbContainer = None
+    app: fastapi.FastAPI = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mongo = MongoDbContainer(MONGO_CONTAINER_VERSION)
+        cls.mongo.start()
 
         s = Settings(
             mongo=mongox.Settings(
-                url=self.mongo.get_connection_url(),
+                url=cls.mongo.get_connection_url(),
             )
         )
 
-        self.app = create_app(
+        cls.app = create_app(
             world.crud_router,
             settings_override=s,
         )
-        self.app.dependency_overrides[get_settings] = lambda: s
+        cls.app.dependency_overrides[get_settings] = lambda: s
 
-    async def asyncTearDown(self) -> None:
-        self.mongo.stop()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.mongo.stop()
+
+    def run(
+        self, result: unittest.result.TestResult | None = ...
+    ) -> unittest.result.TestResult | None:
+        with TestClient(self.app) as client:
+            self.client = client
+            super().run(result)
 
     async def test_create(self):
-        with TestClient(self.app) as client:
-            world_name = "test_world_name"
-            resp = client.post(
-                f"/api/world/{world_name}/create",
-                json=WorldCreateRequest().dict(),
-            )
+        world_name = "test_world_name"
+        resp = self.client.post(
+            f"/api/world/{world_name}/create",
+            json=WorldCreateRequest().dict(),
+        )
 
-            self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 200)
 
-            # post same again
-            resp = client.post(
-                f"/api/world/{world_name}/create",
-                json=WorldCreateRequest().dict(),
-            )
-            self.assertEqual(resp.status_code, 400)
+        # post same again
+        resp = self.client.post(
+            f"/api/world/{world_name}/create",
+            json=WorldCreateRequest().dict(),
+        )
+        self.assertEqual(resp.status_code, 400)
