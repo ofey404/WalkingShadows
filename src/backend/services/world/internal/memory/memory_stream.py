737ac298103698f8d6835e68708528fa9e45e55c
Ofey Chan <ofey206@gmail.com>
2023-05-10 21:54:09 +0800

    stage: a tested memorystream

--- src/backend/services/world/internal/memory/memory_stream.py
+++ src/backend/services/world/internal/memory/memory_stream.py
@@ -1,10 +1,10 @@
 from copy import deepcopy
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from langchain.schema import BaseRetriever, Document
 from langchain.vectorstores import VectorStore
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 
 def _get_hours_passed(time: datetime, ref_time: datetime) -> float:
@@ -12,12 +12,13 @@ def _get_hours_passed(time: datetime, ref_time: datetime) -> float:
     return (time - ref_time).total_seconds() / 3600
 
 
-class MemoryStream(BaseRetriever):
+class MemoryStream(BaseRetriever, BaseModel):
     """based on langchain's TimeWeightedVectorStoreRetriever,
     Retriever combining embedding similarity with recency.
     """
 
-    """Retriever combining embededing similarity with recency."""
+    get_timestamp: Callable[[], int]
+    """The function to get the current timestamp, required."""
 
     vectorstore: VectorStore
     """The vectorstore to store documents and determine salience."""
@@ -51,25 +52,27 @@ class MemoryStream(BaseRetriever):
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         """Return documents that are relevant to the query."""
-        current_time = datetime.now()
+        ts = self.get_timestamp()
         docs_and_scores = {
             doc.metadata["buffer_idx"]: (doc, self.default_salience)
             for doc in self.memory_stream[-self.k :]
         }
+
         # If a doc is considered salient, update the salience score
         docs_and_scores.update(self.get_salient_docs(query))
         rescored_docs = [
-            (doc, self._get_combined_score(doc, relevance, current_time))
+            (doc, self._get_combined_score(doc, relevance, ts))
             for doc, relevance in docs_and_scores.values()
         ]
         rescored_docs.sort(key=lambda x: x[1], reverse=True)
         result = []
+
         # Ensure frequently accessed memories aren't forgotten
-        current_time = datetime.now()
+        ts = self.get_timestamp()
         for doc, _ in rescored_docs[: self.k]:
             # TODO: Update vector store doc once `update` method is exposed.
             buffered_doc = self.memory_stream[doc.metadata["buffer_idx"]]
-            buffered_doc.metadata["last_accessed_at"] = current_time
+            buffered_doc.metadata["last_accessed_at"] = ts
             result.append(buffered_doc)
         return result
 
@@ -81,14 +84,10 @@ class MemoryStream(BaseRetriever):
         self,
         document: Document,
         vector_relevance: Optional[float],
-        current_time: datetime,
+        ts: int,
     ) -> float:
         """Return the combined score for a document."""
-        hours_passed = _get_hours_passed(
-            current_time,
-            document.metadata["last_accessed_at"],
-        )
-        score = (1.0 - self.decay_rate) ** hours_passed
+        score = (1.0 - self.decay_rate) ** (ts - document.metadata["last_accessed_at"])
         for key in self.other_score_keys:
             if key in document.metadata:
                 score += document.metadata[key]
@@ -107,34 +106,41 @@ class MemoryStream(BaseRetriever):
             buffer_idx = fetched_doc.metadata["buffer_idx"]
             doc = self.memory_stream[buffer_idx]
             results[buffer_idx] = (doc, relevance)
+        # TODO: refactor this.
+        #       those data have schema, but they ignore it and dump them into dict.
+        #       use pydantic to parse them.
         return results
 
     def add_documents(self, documents: List[Document], **kwargs: Any) -> List[str]:
         """Add documents to vectorstore."""
-        current_time = kwargs.get("current_time", datetime.now())
+        ts = kwargs.get("ts", self.get_timestamp())
+
         # Avoid mutating input documents
         dup_docs = [deepcopy(d) for d in documents]
-        for i, doc in enumerate(dup_docs):
-            if "last_accessed_at" not in doc.metadata:
-                doc.metadata["last_accessed_at"] = current_time
-            if "created_at" not in doc.metadata:
-                doc.metadata["created_at"] = current_time
-            doc.metadata["buffer_idx"] = len(self.memory_stream) + i
-        self.memory_stream.extend(dup_docs)
+        self._add_documents_to_memory_stream(dup_docs, ts)
         return self.vectorstore.add_documents(dup_docs, **kwargs)
 
     async def aadd_documents(
         self, documents: List[Document], **kwargs: Any
     ) -> List[str]:
-        """Add documents to vectorstore."""
-        current_time = kwargs.get("current_time", datetime.now())
+        """Async add documents to vectorstore."""
+        ts = kwargs.get("ts", self.get_timestamp())
+
         # Avoid mutating input documents
         dup_docs = [deepcopy(d) for d in documents]
-        for i, doc in enumerate(dup_docs):
-            if "last_accessed_at" not in doc.metadata:
-                doc.metadata["last_accessed_at"] = current_time
-            if "created_at" not in doc.metadata:
-                doc.metadata["created_at"] = current_time
-            doc.metadata["buffer_idx"] = len(self.memory_stream) + i
-        self.memory_stream.extend(dup_docs)
+        self._add_documents_to_memory_stream(dup_docs, ts)
         return await self.vectorstore.aadd_documents(dup_docs, **kwargs)
+
+    def _add_documents_to_memory_stream(
+        self,
+        documents: List[Document],
+        ts: int,
+    ):
+        """Add documents to vectorstore."""
+        for i, doc in enumerate(documents):
+            if "last_accessed_at" not in doc.metadata:
+                doc.metadata["last_accessed_at"] = ts
+            if "created_at" not in doc.metadata:
+                doc.metadata["created_at"] = ts
+            doc.metadata["buffer_idx"] = len(self.memory_stream) + i
+        self.memory_stream.extend(documents)
